4ab287d12120fbb16f968137ac82871666efd00b
VarunChavda78 <155780677+VarunChavda78@users.noreply.github.com>
2025-07-28 15:34:43 +0530

    Update output.tf

--- Terraform7/outputs.tf
+++ Terraform7/outputs.tf
@@ -2,7 +2,3 @@ output "alb_url" {
   description = "Public URL of the ALB to access Strapi"
   value       = aws_lb.strapi_alb.dns_name
 }
-
-output "ecr_image_url" {
-  value = "${aws_ecr_repository.strapi_repo.repository_url}:latest"
-}
