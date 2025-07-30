039af99bd0b63d70fa56db57dc703a2ed9d1e4f2
VarunChavda78 <155780677+VarunChavda78@users.noreply.github.com>
2025-07-30 16:07:19 +0530

    Task 10

--- Strapi-app/src/admin/vite.config.example.ts
+++ Strapi-app/src/admin/vite.config.example.ts
@@ -1,10 +1,12 @@
 import { mergeConfig, type UserConfig } from 'vite';
- 
+
 export default (config: UserConfig) => {
   // Important: always return the modified config
   return mergeConfig(config, {
-    server: {
-      allowedHosts: true,
+    resolve: {
+      alias: {
+        '@': '/src',
+      },
     },
   });
 };
