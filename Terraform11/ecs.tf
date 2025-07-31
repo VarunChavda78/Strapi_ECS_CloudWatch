c737c61b2db66e6f5e2708b7cea87191c5a55a56
VarunChavda78 <155780677+VarunChavda78@users.noreply.github.com>
2025-07-31 21:43:14 +0530

    update ecs

--- Terraform11/ecs.tf
+++ Terraform11/ecs.tf
@@ -45,7 +45,7 @@ resource "aws_iam_role" "codedeploy_role" {
 
 resource "aws_iam_role_policy_attachment" "codedeploy_role_policy" {
   role       = aws_iam_role.codedeploy_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSCodeDeployRoleForECS"
+  policy_arn = "arn:aws:iam::aws:policy/AWSCodeDeployRoleForECS"
 }
 
 
